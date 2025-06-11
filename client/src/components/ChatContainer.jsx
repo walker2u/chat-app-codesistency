4ad8c8d59745ae2722456cbd6418415d8a93a329
walker2u <kumar99prasad@gmail.com>
2025-06-12 00:11:10 +0530

    showing message in mesage container, rest is to implement socket io.

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -3,10 +3,12 @@ import { useChatStore } from "../store/useChatStore.js";
 import ChatHeader from "../components/ChatHeader.jsx";
 import ChatInput from "../components/ChatInput.jsx";
 import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
+import { useAuthStore } from "../store/useAuthStore.js";
 
 function ChatContainer() {
   const { messages, selectedUser, isMessagesLoading, getMessages, users } =
     useChatStore();
+  const { authUser } = useAuthStore();
   useEffect(() => {
     getMessages(selectedUser._id);
   }, []);
@@ -23,7 +25,44 @@ function ChatContainer() {
   return (
     <div className="flex flex-1 flex-col overflow-auto">
       <ChatHeader />
-      <p>messages...</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${
+              message.senderId === authUser._id ? "chat-end" : "chat-start"
+            }`}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.png"
+                      : selectedUser.profilePic || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {message.createdAt}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
       <ChatInput />
     </div>
   );
